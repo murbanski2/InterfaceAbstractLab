750f73658a1fcf2eb84d4b269a057b188c42dee7
murbanski@my.wctc.edu <>
2013-01-30 21:27:05 -0600

    Changes to Course.java to include all of the fields and methods.  I will make things abstract later.

--- src/lab1/Course.java
+++ src/lab1/Course.java
@@ -4,10 +4,75 @@
  */
 package lab1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Mark Urbanski
  */
 public class Course {
+    private String courseName; 
+    private String courseNumber;
+    private double credits;
+    private String prerequisites; //make sure this is set to 'None' for Intro to programming.
     
+     public Course(String courseName, String courseNumber) {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public final void setCourseName(String courseName) {
+        //I should use a nullOrZeroLength() function to test string.
+        //Use for all string entries.
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
+    
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public final void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseNumber = courseNumber;
+    }
+    
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.credits = credits;
+    }
+    
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
+            System.exit(0);
+        }
+        this.prerequisites = prerequisites;
+    }
+
 }
