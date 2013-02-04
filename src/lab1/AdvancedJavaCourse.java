f626b1620d39412f34606bf921dcce7f95cdabaf
murbanski@my.wctc.edu <>
2013-02-03 23:34:16 -0600

    Finished AdvancedJavaCourse

--- src/lab1/AdvancedJavaCourse.java
+++ src/lab1/AdvancedJavaCourse.java
@@ -4,11 +4,13 @@ import javax.swing.JOptionPane;
 
 /**
  * Describe responsibilities here.
- *
+ * AdvancedJavaCourse must provide setters and getters as determined by Course.
+ * That means courseName, courseNumber, credits, and prerequisites.
+ * I am also including the member properties here, all private.
  * @author      Mark Urbanski
  * @version     1.00
  */
-public class AdvancedJavaCourse {
+public class AdvancedJavaCourse extends Course{
     private String courseName; //this should be private
     private String courseNumber;
     private double credits;
@@ -24,10 +26,12 @@ public class AdvancedJavaCourse {
     }
 
 
+    @Override
     public String getPrerequisites() {
         return prerequisites;
     }
 
+    @Override
     public void setPrerequisites(String prerequisites) {
         if(prerequisites == null || prerequisites.length() == 0) {
             JOptionPane.showMessageDialog(null,
@@ -37,6 +41,7 @@ public class AdvancedJavaCourse {
         this.prerequisites = prerequisites;
     }
 
+    @Override
     public void setCredits(double credits) {
         if(credits < 0.5 || credits > 4.0) {
             JOptionPane.showMessageDialog(null,
@@ -46,20 +51,29 @@ public class AdvancedJavaCourse {
         this.setCredits(credits);
     }
 
+    @Override
     public String getCourseName() {
         return courseName;
     }
 
+    @Override
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
 
+    @Override
     public String getCourseNumber() {
         return courseNumber;
     }
 
+    @Override
     public void setCourseNumber(String courseNumber) {
         this.courseNumber = courseNumber;
+    }
+
+    @Override
+    public double getCredits() {
+        return credits;
     }
     
 }
