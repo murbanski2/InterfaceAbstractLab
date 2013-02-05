7e33fe464f8fca5854d709d23e370dfdc53b0412
murbanski@my.wctc.edu <>
2013-02-04 18:28:16 -0600

    Changes needed for IntroToProgrammingCourse

--- src/lab2/IntroToProgrammingCourse.java
+++ src/lab2/IntroToProgrammingCourse.java
@@ -3,16 +3,23 @@ package lab2;
 import javax.swing.JOptionPane;
 
 /**
- * Describe responsibilities here.
+ * Describe responsibilities here. NOTE: this class has NO PREREQUISITES!
+ * Do not change this fact.
+ * IntroToProgrammingCourse must provide setters and getters as determined by Course.
+ * That means courseName, courseNumber, credits, and prerequisites.
+ * I am also including 3 member properties here.  Setter for prerequisite will
+ * do nothing.  I personally think it should have a prerequisite field set to None.
+ * The getter will just return None.
  *
- * @author      your name goes here
+ * @author      Mark Urbanski
  * @version     1.00
  */
-public class IntroToProgrammingCourse {
+public class IntroToProgrammingCourse implements Course{
     private String courseName;
     private String courseNumber;
     private double credits;
-
+    //should I have a prerequisite here, set to none?  Instructions say do not add here.
+    
     public IntroToProgrammingCourse(String courseName, String courseNumber) {
         this.setCourseName(courseName);
         this.setCourseNumber(courseNumber);
@@ -22,6 +29,7 @@ public class IntroToProgrammingCourse {
         return courseNumber;
     }
 
+    @Override
     public final void setCourseNumber(String courseNumber) {
         if(courseNumber == null || courseNumber.length() == 0) {
             JOptionPane.showMessageDialog(null,
@@ -31,10 +39,12 @@ public class IntroToProgrammingCourse {
         this.courseNumber = courseNumber;
     }
 
+    @Override
     public double getCredits() {
         return credits;
     }
 
+    @Override
     public void setCredits(double credits) {
         if(credits < 0.5 || credits > 4.0) {
             JOptionPane.showMessageDialog(null,
@@ -44,10 +54,12 @@ public class IntroToProgrammingCourse {
         this.credits = credits;
     }
 
+    @Override
     public String getCourseName() {
         return courseName;
     }
 
+    @Override
     public final void setCourseName(String courseName) {
         if(courseName == null || courseName.length() == 0) {
             JOptionPane.showMessageDialog(null,
@@ -55,6 +67,17 @@ public class IntroToProgrammingCourse {
             System.exit(0);
         }
         this.courseName = courseName;
+    }
+
+    @Override
+    public String getPrerequisites() {
+        return "None";
+        
+    }
+
+    @Override
+    public void setPrerequisites(String prerequisites) {
+        //Do nothing with the prerequisite.  It doesn't have one.
     }
 
     
