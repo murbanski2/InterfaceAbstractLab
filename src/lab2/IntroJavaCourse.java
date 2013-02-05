4df96acf6558dae5ef54977c0195fbc5b1c9348a
murbanski@my.wctc.edu <>
2013-02-04 18:26:17 -0600

    Changes needed for IntroJavaCourse

--- src/lab2/IntroJavaCourse.java
+++ src/lab2/IntroJavaCourse.java
@@ -1,13 +1,18 @@
 package lab2;
 
+
 /**
  * Describe responsibilities here.
+ * IntroJavaCourse must provide setters and getters as determined by Course.
+ * That means courseName, courseNumber, credits, and prerequisites.
+ * I am also including the member properties here, all private.
  *
- * @author      your name goes here
+ * @author      Mark Urbanski
  * @version     1.00
  */
-public class IntroJavaCourse {
-    String courseName;
+public class IntroJavaCourse implements Course{
+
+    private String courseName; //should be private
     private String courseNumber;
     private double credits;
     private String prerequisites;
@@ -29,9 +34,6 @@ public class IntroJavaCourse {
         return credits;
     }
 
-    public void setCredits(double credits) {
-        this.credits = credits;
-    }
 
     public String getPrerequisites() {
         return prerequisites;
@@ -41,5 +43,25 @@ public class IntroJavaCourse {
         this.prerequisites = prerequisites;
     }
 
+    public void setCredits(double credits) {
+        //I am changing the test to match the output.  Originally, it would allow
+        //a range of 0 - 5.  I am making the choice here that the output
+        //was correct.  The problem requirements would tell me which was correct.
+        if(credits < 0.5 || credits > 4.0) {
+            System.out.println(
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.credits = credits;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+    
     
 }
